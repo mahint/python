657379e85938987da5347710e61d84c3b0d1fa38
mahint <58563940+mahint@users.noreply.github.com>
2020-01-19 20:10:39 +0100

    initial import

---
+++ odd_even.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+# Author Mahin
+#Tells you if the entered number is odd or even
+x = int(input("Number:"))
+if (x % 2) == 0:
+    print("Its Even")
+elif (x % 2) == 1:
+    print ("Its Odd")
