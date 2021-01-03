bc7c24991c28cd5be3fe315db6386ff24cd7249d
mahint <58563940+mahint@users.noreply.github.com>
2021-01-03 15:30:20 +0100

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -13,19 +13,31 @@ HALF_PAD_WIDTH = PAD_WIDTH / 2
 HALF_PAD_HEIGHT = PAD_HEIGHT / 2
 LEFT = False
 RIGHT = True
-ball_pos = [WIDTH / 2, HEIGHT / 2]
-vel = [-5, 1]
+vert_vel_l = - random.randrange(2, 5)
+hori_vel_l = - random.randrange(2, 5)
+vert_vel_r = - random.randrange(2, 5)
+hori_vel_r =  random.randrange(2, 5)
 # initialize ball_pos and ball_vel for new bal in middle of table
 # if direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
     global ball_pos, ball_vel # these are vectors stored as lists
-
+    ball_pos = [WIDTH / 2, HEIGHT / 2]
+    direction = random.randrange(1, 3)    
+    if direction == 1:
+        direction = LEFT
+    elif direction == 2:
+        direction = RIGHT
+    if direction == LEFT:
+        ball_vel = [hori_vel_l, vert_vel_l]
+    elif direction == RIGHT:
+        ball_vel = [hori_vel_r, vert_vel_r]
 
 # define event handlers
 def new_game():
     global paddle1_pos, paddle2_pos, paddle1_vel, paddle2_vel  # these are numbers
     global score1, score2  # these are ints
-
+    spawn_ball(new_game)
+    
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
  
@@ -36,8 +48,13 @@ def draw(canvas):
     canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
         
     # update ball
-    ball_pos[0] += vel[0]
-    ball_pos[1] += vel[1]
+    ball_pos[0] += ball_vel[0]
+    ball_pos[1] += ball_vel[1]
+    
+    #if ball_pos == LEFT:
+    #    ball_pos = [WIDTH / 2, HEIGHT / 2]
+    #elif ball_pos == RIGHT:
+    #    ball_pos = [WIDTH / 2, HEIGHT / 2]
     # draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "White", "White")
     # update paddle's vertical position, keep paddle on the screen
@@ -46,13 +63,13 @@ def draw(canvas):
     
     # determine whether paddle and ball collide    
     if ball_pos[0] <= BALL_RADIUS:
-        vel[0] = - vel[0]
+        ball_vel[0] = - ball_vel[0]
     elif ball_pos[0] >= WIDTH - BALL_RADIUS:
-        vel[0] = - vel[0]
+        ball_vel[0] = - ball_vel[0]
     elif ball_pos[1] <= BALL_RADIUS:
-        vel[1] = - vel[1]
+        ball_vel[1] = - ball_vel[1]
     elif ball_pos[1] >= HEIGHT - BALL_RADIUS:
-        vel[1] = - vel[1]
+        ball_vel[1] = - ball_vel[1]
     # draw scores
         
 def keydown(key):
