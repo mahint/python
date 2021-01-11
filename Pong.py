29ca51891840dca99dc66098a8a2bed745ce3bf3
mahint <58563940+mahint@users.noreply.github.com>
2021-01-11 18:54:31 +0100

    Update Pong.py

--- Pong.py
+++ Pong.py
@@ -17,6 +17,11 @@ vert_vel_l = - random.randrange(2, 4)
 hori_vel_l = - random.randrange(2, 4)
 vert_vel_r = - random.randrange(2, 4)
 hori_vel_r =  random.randrange(2, 4)
+paddle1_pos = [PAD_WIDTH, PAD_HEIGHT, PAD_WIDTH, HEIGHT / 2]
+paddle1_vel = [4, 4]
+paddle2_pos = [WIDTH - PAD_WIDTH, 0, WIDTH - PAD_WIDTH, HEIGHT]
+paddle2_vel = [4, 4]
+
 # initialize ball_pos and ball_vel for new bal in middle of table
 # if direction is RIGHT, the ball's velocity is upper right, else upper left
 def spawn_ball(direction):
@@ -40,15 +45,11 @@ def new_game():
     
 def draw(canvas):
     global score1, score2, paddle1_pos, paddle2_pos, ball_pos, ball_vel
- 
         
     # draw mid line and gutters
     canvas.draw_line([WIDTH / 2, 0],[WIDTH / 2, HEIGHT], 1, "White")
     canvas.draw_line([PAD_WIDTH, 0],[PAD_WIDTH, HEIGHT], 1, "White")
-    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")
-    canvas.draw_line([PAD_WIDTH, PAD_HEIGHT], [PAD_WIDTH, HEIGHT / 2], PAD_WIDTH, "White")
-    canvas.draw_line([WIDTH - PAD_WIDTH, PAD_HEIGHT], [WIDTH - PAD_WIDTH, HEIGHT / 2], PAD_WIDTH, "White")
-   
+    canvas.draw_line([WIDTH - PAD_WIDTH, 0],[WIDTH - PAD_WIDTH, HEIGHT], 1, "White")   
     # update ball
     ball_pos[0] += ball_vel[0]
     ball_pos[1] += ball_vel[1]
@@ -64,9 +65,11 @@ def draw(canvas):
     # draw ball
     canvas.draw_circle(ball_pos, BALL_RADIUS, 2, "White", "White")
     # update paddle's vertical position, keep paddle on the screen
-    
+    paddle1_pos[1] += paddle1_vel[1]
+    paddle2_pos[1] += paddle2_vel[1]
     # draw paddles
-    
+    canvas.draw_line([PAD_WIDTH, PAD_HEIGHT], [PAD_WIDTH, HEIGHT / 2], PAD_WIDTH, "White")
+    canvas.draw_line([WIDTH - PAD_WIDTH, PAD_HEIGHT], [WIDTH - PAD_WIDTH, HEIGHT / 2], PAD_WIDTH, "White")
     # determine whether paddle and ball collide    
     if ball_pos[0] <= BALL_RADIUS:
         ball_vel[0] = - ball_vel[0]
@@ -80,7 +83,7 @@ def draw(canvas):
         
 def keydown(key):
     global paddle1_vel, paddle2_vel
-   
+    
 def keyup(key):
     global paddle1_vel, paddle2_vel
 
