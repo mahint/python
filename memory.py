8592c22aee4e57050b9721415822583867475a70
mahint <58563940+mahint@users.noreply.github.com>
2021-02-26 20:28:52 +0100

    Update memory.py

--- memory.py
+++ memory.py
@@ -31,10 +31,23 @@ def mouseclick(pos):
 def draw(canvas):
     for card_index in range(len(cards)):
         card_pos = 50 * card_index
-        canvas.draw_text(str(cards[card_index]), [card_pos, 65], 48, "White")
-        canvas.draw_polygon([[0, 0], [50, 0], [50, 100], [0, 100]], 50, "Green", "Red")
-    
-    
+        canvas.draw_text(str(cards[card_index]), [12 + card_pos, 65], 48, "White")
+        canvas.draw_polygon([[0, 0], [CARD_WIDTH, 0], [CARD_WIDTH, CARD_HEIGHT], [0, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[CARD_WIDTH, 0], [2*CARD_WIDTH, 0], [2*CARD_WIDTH, CARD_HEIGHT], [CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[2*CARD_WIDTH, 0], [3*CARD_WIDTH, 0], [3*CARD_WIDTH, CARD_HEIGHT], [2*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[3*CARD_WIDTH, 0], [4*CARD_WIDTH, 0], [4*CARD_WIDTH, CARD_HEIGHT], [3*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[4*CARD_WIDTH, 0], [5*CARD_WIDTH, 0], [5*CARD_WIDTH, CARD_HEIGHT], [4*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[5*CARD_WIDTH, 0], [6*CARD_WIDTH, 0], [6*CARD_WIDTH, CARD_HEIGHT], [5*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[6*CARD_WIDTH, 0], [7*CARD_WIDTH, 0], [7*CARD_WIDTH, CARD_HEIGHT], [6*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[7*CARD_WIDTH, 0], [8*CARD_WIDTH, 0], [8*CARD_WIDTH, CARD_HEIGHT], [7*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[8*CARD_WIDTH, 0], [9*CARD_WIDTH, 0], [9*CARD_WIDTH, CARD_HEIGHT], [8*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[9*CARD_WIDTH, 0], [10*CARD_WIDTH, 0], [10*CARD_WIDTH, CARD_HEIGHT], [9*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[10*CARD_WIDTH, 0], [11*CARD_WIDTH, 0], [11*CARD_WIDTH, CARD_HEIGHT], [10*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[11*CARD_WIDTH, 0], [12*CARD_WIDTH, 0], [12*CARD_WIDTH, CARD_HEIGHT], [11*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[12*CARD_WIDTH, 0], [13*CARD_WIDTH, 0], [13*CARD_WIDTH, CARD_HEIGHT], [12*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[13*CARD_WIDTH, 0], [14*CARD_WIDTH, 0], [14*CARD_WIDTH, CARD_HEIGHT], [13*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[14*CARD_WIDTH, 0], [15*CARD_WIDTH, 0], [15*CARD_WIDTH, CARD_HEIGHT], [14*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
+        canvas.draw_polygon([[15*CARD_WIDTH, 0], [16*CARD_WIDTH, 0], [16*CARD_WIDTH, CARD_HEIGHT], [15*CARD_WIDTH, CARD_HEIGHT]], 1, "Red", "Green")
 # create frame and add a button and labels
 frame = simplegui.create_frame("Memory", 800, 100)
 frame.add_button("Reset", new_game)
