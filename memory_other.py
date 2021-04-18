5fcc3200a78e7cdcd83831d26197653a4dd5c2e6
mahint <58563940+mahint@users.noreply.github.com>
2021-04-18 21:16:08 +0200

    Update memory_other.py
    moved card drawing to init_cards method and spent some time in mouseclick

--- memory_other.py
+++ memory_other.py
@@ -1,4 +1,3 @@
-  
 # implementation of card game - Memory
 
 import simplegui
@@ -80,75 +79,15 @@ def new_game():
      
 # define event handlers
 def mouseclick(pos):
-    print(pos)
+    print("pos: " + str(pos))
     # add game state logic here
     global CARD1_WIDTH, CARD2_WIDTH, CARD2_WIDTH_2, CARD3_WIDTH, CARD3_WIDTH_2, CARD4_WIDTH, CARD4_WIDTH_2, CARD5_WIDTH, CARD5_WIDTH_2, CARD6_WIDTH, CARD6_WIDTH_2, CARD7_WIDTH, CARD7_WIDTH_2, CARD8_WIDTH, CARD8_WIDTH_2, CARD9_WIDTH, CARD9_WIDTH_2, CARD10_WIDTH, CARD10_WIDTH_2, CARD11_WIDTH, CARD11_WIDTH_2, CARD12_WIDTH, CARD12_WIDTH_2, CARD13_WIDTH, CARD13_WIDTH_2, CARD14_WIDTH, CARD14_WIDTH_2, CARD15_WIDTH, CARD15_WIDTH_2, CARD16_WIDTH, CARD16_WIDTH_2
-    print(pos[0] // 50)
-    if pos[0] // 50
-    #    if pos[0] < CARD_WIDTH:
-#        CARD1_WIDTH = 0
-#        print "WORKS"
-#    elif (pos[0] > 50) and (pos[0] < 100):
-#        CARD2_WIDTH = 0
-#        CARD2_WIDTH_2 = 0
-#        print "WORKS"
-#    elif (pos[0] > 100) and (pos[0] < 150):
-#        CARD3_WIDTH = 0
-#        CARD3_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 150) and (pos[0] < 200):
-#        CARD4_WIDTH = 0
-#        CARD4_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 200) and (pos[0] < 250):
-#        CARD5_WIDTH = 0
-#        CARD5_WIDTH_2 = 0
-#       
-#    elif (pos[0] > 250) and (pos[0] < 300):
-#        CARD6_WIDTH = 0
-#        CARD6_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 300) and (pos[0] < 350):
-#        CARD7_WIDTH = 0
-#        CARD7_WIDTH_2 = 0
-#     
-#    elif (pos[0] > 350) and (pos[0] < 400):
-#        CARD8_WIDTH = 0
-#        CARD8_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 400) and (pos[0] < 450):
-#        CARD9_WIDTH = 0
-#        CARD9_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 450) and (pos[0] < 500):
-#        CARD10_WIDTH = 0
-#        CARD10_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 500) and (pos[0] < 550):
-#        CARD11_WIDTH = 0
-#        CARD11_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 550) and (pos[0] < 600):
-#        CARD12_WIDTH = 0
-#        CARD12_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 600) and (pos[0] < 650):
-#        CARD13_WIDTH = 0
-#        CARD13_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 650) and (pos[0] < 700):
-#        CARD14_WIDTH = 0
-#        CARD14_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 700) and (pos[0] < 750):
-#        CARD15_WIDTH = 0
-#        CARD15_WIDTH_2 = 0
-#        
-#    elif (pos[0] > 750) and (pos[0] < 800):
-#        CARD16_WIDTH = 0
-#        CARD16_WIDTH_2 = 0
-# cards are logically 50x100 pixels in size    
-def draw(canvas):
+    print("card pos: " + str(pos[0] // 50))
+    CARD_WIDTH = 0
+    #CARD2_WIDTH_2 = 0
+    
+
+def init_cards(canvas):
     for card_index in range(len(cards)):
         card_pos = 50 * card_index
         #print(card_pos)
@@ -173,6 +112,8 @@ def draw(canvas):
                 ], FONT_SIZE, "Red", "Green"
             )
 
+def draw(canvas):
+    init_cards(canvas)
     #for exposed_index in exposed:
         #if exposed_index == 
 # create frame and add a button and labels
